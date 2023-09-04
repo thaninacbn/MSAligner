dccf28c61149555a49b6066bebde96bebe6bcde0
Thanina Chabane <thaninacbn@gmail.com>
2023-09-04 11:44:55 +0200

    outline the main algo

---
+++ src/main.py
@@ -0,0 +1,17 @@
+# define main functions
+
+def read_fasta(files):
+    #will read protein fasta files
+
+def pairwise_alignment(seq1, seq2):
+    #runs pairwise alignment using needleman-wunsch algorithm
+
+def calculate_score (sequences):
+    #creates a scores matrix
+
+def create_guide_tree(sequences, matrix):
+    #creates guide tree
+
+def run_multiple_alignment(sequences, tree)
+    #uses tree to align the sequences
+
