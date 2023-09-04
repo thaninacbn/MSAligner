4e4c57462e9b5ba1256196b8cb694c435a2400f2
Thanina Chabane <thaninacbn@gmail.com>
2023-09-04 12:07:23 +0200

    add outline of read_fasta

--- src/main.py
+++ src/main.py
@@ -1,17 +1,31 @@
 # define main functions
 
-def read_fasta(files):
-    #will read protein fasta files
+def read_fasta(fasta_file):
+    #code from previous python exos
+    with open(fasta_file, "r") as filin:
+        sequence_list = []
+        for line in filin:
+            if line.startswith(">"):
+                pass
+            else:
+                read = filin.readline()
+                read = read.strip()
+                sequence_list.append(read)
+                sequence = "".join(sequence_list)
+    return sequence
+
+
+
 
 def pairwise_alignment(seq1, seq2):
-    #runs pairwise alignment using needleman-wunsch algorithm
+    # runs pairwise alignment using needleman-wunsch algorithm
 
 def calculate_score (sequences):
-    #creates a scores matrix
+    # creates a scores matrix
 
 def create_guide_tree(sequences, matrix):
-    #creates guide tree
+    # creates guide tree
 
 def run_multiple_alignment(sequences, tree)
-    #uses tree to align the sequences
+    # uses tree to align the sequences
 
